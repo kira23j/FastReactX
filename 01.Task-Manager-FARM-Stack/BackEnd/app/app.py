4240fa1dc389befbdb9b99e9cef3596907451003
kira23j <kirubelmamuyesahle@gmail.com>
2024-02-06 21:50:35 +0300

    adding get request for from fastapi to react

--- 01.Task-Manager-FARM-Stack/BackEnd/app/app.py
+++ 01.Task-Manager-FARM-Stack/BackEnd/app/app.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app=FastAPI()
 
 origins=[
-    "http:localhost:3000",
+    "http://localhost:3000",
     "localhost:3000"
 ]
 app.add_middleware(
@@ -18,4 +18,20 @@ app.add_middleware(
 @app.get("/",tags=["root"])
 async def read_root()->dict:
     return {"message":"welcome to fastapi"}
-    
+
+
+todos=[
+    {"id":"1",
+     "item":"something 1"
+        },
+  {"id":"2",
+     "item":"something 2"
+        },
+  {"id":"3",
+     "item":"something 3"
+        },
+]
+# get todo route
+@app.get("/todo",tags=["todos"])
+async def get_todos()->dict:
+    return {"data":todos}
--- 01.Task-Manager-FARM-Stack/FrontEnd/src/index.jsx
+++ 01.Task-Manager-FARM-Stack/FrontEnd/src/index.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { render } from "react-dom";
 import {ChakraProvider} from "@chakra-ui/react";
 import Header from  "./components/Header"
-
+import Todos from "./components/Todos"
 function App(){
   return (
     <ChakraProvider>
       <Header />
+      <Todos/>
       
     </ChakraProvider>
   )
