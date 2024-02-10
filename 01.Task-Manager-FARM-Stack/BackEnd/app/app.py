bdb5f2318fdca17b4a04e3d5f109fa47d535a931
kira23j <kirubelmamuyesahle@gmail.com>
2024-02-10 10:53:50 -0800

    adding features with put method

--- 01.Task-Manager-FARM-Stack/BackEnd/app/app.py
+++ 01.Task-Manager-FARM-Stack/BackEnd/app/app.py
@@ -44,3 +44,16 @@ async def add_todo(todo:dict)->dict:
         "data":{"Todo has been added!"}
     }
     
+# put route
+@app.put("/todo/{id}",tags=["todos"] )
+async def update_todo(id:int,body:dict)->dict:
+    for todo in todos:
+        if int(todo["id"])==id:
+            todo["item"]=body["item"]
+            return {
+                "data":f"Todo with id {id} has been updated"
+            }
+    return {
+            "data":f"Todo with this {id} number has not been found!"
+        }
+        
