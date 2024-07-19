ae8d4752d6db94792eeee731b47d47336bb2f7b3
kira23j <kirubelmamuyesahle@gmail.com>
2024-07-19 10:12:48 -0700

    adding final source code for fast api and react task manager app

--- 01.Task-Manager-FastAPI & React/React-FrontEnd/src/index.jsx
+++ 01.Task-Manager-FastAPI & React/React-FrontEnd/src/index.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { render } from "react-dom";
-import {ChakraProvider} from "@chakra-ui/react";
-import Header from  "./components/Header"
+import { render } from 'react-dom';
+import { ChakraProvider } from "@chakra-ui/react";
+
+import Header from "./components/Header";
 import Todos from "./components/Todos"
-function App(){
+
+function App() {
   return (
     <ChakraProvider>
       <Header />
-      <Todos/>
-      
+      <Todos />
     </ChakraProvider>
   )
 }
-const rootElement=document.getElementById("root")
-render(<App/>,rootElement)
+
+const rootElement = document.getElementById("root")
+render(<App />, rootElement)
