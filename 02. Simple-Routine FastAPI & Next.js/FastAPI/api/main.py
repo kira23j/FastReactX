dbb6bb59825923cc0664e63c943fca2ffd03bca1
kira23j <kirubelmamuyesahle@gmail.com>
2024-07-31 15:33:22 -0700

    configuring database and debugging error

--- 02. Simple-Routine FastAPI & Next.js/FastAPI/api/main.py
+++ 02. Simple-Routine FastAPI & Next.js/FastAPI/api/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routers import auth
 
+from database import Base,engine
 app=FastAPI()
+Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,3 +17,5 @@ app.add_middleware(
 @app.get("/")
 def health_check():
     return "health is complete"
+
+app.include_router(auth.router)
--- 02. Simple-Routine FastAPI & Next.js/FastAPI/api/models.py
+++ 02. Simple-Routine FastAPI & Next.js/FastAPI/api/models.py
@@ -1,11 +1,11 @@
 from sqlalchemy import Column ,Integer , String , ForeignKey , Table
 from sqlalchemy.orm import relationship
-from .database import Base
+from database import Base
 
 workout_routine_association=Table(
     'workout_routine',Base.metadata,
     Column('workout_id',Integer,ForeignKey('workouts.id')),
-    Column('routine.id',Integer,ForeignKey('routines.id'))
+    Column('routine_id',Integer,ForeignKey('routines.id'))
 )
 
 class User(Base):
@@ -19,14 +19,14 @@ class Workout(Base):
     user_id=Column(Integer,ForeignKey('users.id'))
     name=Column(String,index=True)
     description=Column(String,index=True)
-    routines=relationship('Routine',secoundary=workout_routine_association,back_populates='workouts')
+    routines=relationship('Routine',secondary=workout_routine_association,back_populates='workouts')
 
 class Routine(Base):
     __tablename__='routines'
     id=Column(Integer,primary_key=True,index=True)
     user_id=Column(Integer,ForeignKey('users.id'))
     name=Column(String , index=True)
-    description=Column(String,True)
+    description=Column(String,index=True)
     workouts=relationship('workout',secondary=workout_routine_association,back_populates='routines')
     
 Workout.routines=relationship("Routine",secondary=workout_routine_association,back_populates='workouts')
