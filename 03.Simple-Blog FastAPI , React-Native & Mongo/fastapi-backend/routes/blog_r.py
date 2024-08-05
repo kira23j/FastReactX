47a398175d454648e6660cbca2d26719fe7c27e8
kira23j <kirubelmamuyesahle@gmail.com>
2024-08-05 12:06:37 -0700

    sucessfully posting blog to mongodb database

--- 03.Simple-Blog FastAPI , React-Native & Mongo/fastapi-backend/routes/blog_r.py
+++ 03.Simple-Blog FastAPI , React-Native & Mongo/fastapi-backend/routes/blog_r.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from models.blog_m import BlogModel 
+from config.config import blogs_collection
+import datetime
 
 blog_root=APIRouter()
 
@@ -7,7 +9,16 @@ blog_root=APIRouter()
 @blog_root.post("/new/blog")
 def new_blog(doc:BlogModel):
     doc=dict(doc)
-    return doc
+    current_date=datetime.date.today()
+    doc["date"]=str(current_date)
+    res=blogs_collection.insert_one(doc)
+    doc_id=str(res.inserted_id)
+    return {
+        "status":"ok",
+        "message":"blog posted successfully",
+        "_id":doc_id
+    }
+    
 
     
     
